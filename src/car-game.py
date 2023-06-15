104385b6541fcd291cfc920e3ea9d00f836ade6c
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-06-15 09:48:31 +0600

    Applied OOP in Python

--- src/car-game.py
+++ src/car-game.py
@@ -1,44 +1,60 @@
-print('Welcome to Car game. Type \"help\" to get instructions.')
+print('Welcome to Car game. Type "help" to get instructions.')
 
-started = False
-messages = [
-    "",  # gets added with a new line and puts a blank line
 
-    "Miscellaneous:",
-    "help        - to get this message",
-    "",
+class Car:
+    def __init__(self):
+        self.started = False
+        self.messages = [
+            "",
+            "Miscellaneous:",
+            "help        - to get this message",
+            "",
+            "Game controls:",
+            "start       - to start the car",
+            "stop        - to stop the car",
+            "exit        - to exit from the game",
+            "",
+        ]
 
-    "Game controls:",
-    "start       - to start the car",
-    "stop        - to stop the car",
-    "quit        - to quit from the game",
-    ""
-]
+    def start(self):
+        if self.started:
+            print("Hey! Car is already started.")
+        else:
+            print("Car is started...")
+            self.started = True
+
+    def stop(self):
+        if self.started:
+            print("Car is stopped.")
+            self.started = False
+        else:
+            print("Hey! Car is already stopped.")
+
+    def printHelp(self):
+        for self.message in self.messages:
+            print(self.message)
+
+    def exitCar(self):
+        print("Thanks for playing! :)")
+        exit(0)
+
+
+car = Car()
 
 while True:
     command = input(">> ").lower()
 
     if command == "start":
-        if started:
-            print("Hey! Car is already started.")
-        else:
-            print("Car is started...")
-            started = True
+        car.start()
 
     elif command == "stop":
-        if started:
-            print("Car is stopped.")
-            started = False
-        else:
-            print("Hey! Car is already stopped.")
+        car.stop()
 
     elif command == "help":
-        for message in messages:
-            print(message)
+        car.printHelp()
 
-    elif command == "quit":
-        print("Thanks for playing! :)")
-        break
+    elif command == "exit":
+        car.exitCar()
 
     else:
         print("Sorry, I didn't understand it :(")
